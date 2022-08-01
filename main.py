5a6e2f218ecaf0bd731250994c8ef3e0efc82dc4
Riaz <81258483+R14Z@users.noreply.github.com>
2022-08-01 22:35:19 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+# what i want to do is to create a library for anime and manga first, then be able to add anime and manga to the library
+
+lib = ['One Piece Anime', 'Gintama', 'SBR', 'One Piece Manga', 'Vinland Saga Manga', 'Berserk', 'CSM']
+completed = ['AOT', 'Monster', 'Vagabond', 'Homunculus', 'Jojo Anime', 'Kotaro lives alone', 'Ajin', 'Bleach Anime', 'Fire force Anime'
+, 'Vinland Saga Anime', 'That time I got reincarnated as a slime S1', 'Akame Ga Kill', 'AOT Junior High', 'JJK Anime', 'OPM Anime S1', 
+'Tokyo Revengers Anime S1', 'Akira Movie', 'Blue Exorcist', 'Demon Slayer Mugen Train', 'Demon Slayer Anime S1', 'Bunny Girl Senpai S1',
+'Your Name', 'All of Dragon Ball', 'MHA Anime S1, S2, S3 , S4', 'Saiki K'
+]
+
+question = input('Would you like to add another animanga? (Y,N) ').upper()
+if question == 'Y':
+    y = input('Which animanga would you like to add? ')
+    lib.append(y)
+else:
+    comp_y = input('Would you like to add an animanga to completed? (Y,N)')
+    if comp_y == 'Y':
+        comp_branch = input('Which animanga would you like to be set as completed? ')
+        completed.append(comp_branch)
+        lib.remove(comp_branch)
+    else:
+        print('Goodbye!')
+        
